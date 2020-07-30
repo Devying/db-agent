dcd66512c5bd9785323e1bb73c9d1f6962649a4c
gczmm <zhanglin20@staff.weibo.com>
2020-07-30 18:23:07 +0800

    log

--- server/server.go
+++ server/server.go
@@ -1,18 +1,23 @@
 package server
 
 import (
+	"flag"
 	"github.com/Devying/db-agent/config"
+	vlog "github.com/Devying/db-agent/log"
 	"net"
 	"strconv"
+	"time"
 )
 
-func Init()  {
+func Init() {
 	config.Conf = config.ParseConf()
+	initLog(config.Conf.Log)
 }
 func Start() {
 	var err error
-	l,err := net.Listen("tcp",":"+strconv.Itoa(config.Conf.Port))
+	l, err := net.Listen("tcp", ":"+strconv.Itoa(config.Conf.Port))
 	if err != nil {
+		vlog.Infof("DB agent listening err: %s", err.Error())
 		panic(err)
 	}
 	db := NewDB()
@@ -24,12 +29,22 @@ func Start() {
 	if err != nil {
 		panic(err)
 	}
-	println("listing",config.Conf.Port)
-	for{
-		conn,err := l.Accept()
+	vlog.Infof("DB agent is starting.listening port: %d", config.Conf.Port)
+	for {
+		conn, err := l.Accept()
 		if err != nil {
 			panic(err)
 		}
 		go db.Process(conn)
 	}
+}
+
+func initLog(logdir string) {
+	// TODO: remove after a better handle
+	if logdir == "stdout" {
+		return
+	}
+	_ = flag.Set("log_dir", logdir)
+	vlog.FlushInterval = 1 * time.Second
+	vlog.LogInit(nil)
 }
